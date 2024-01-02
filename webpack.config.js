6ecffc2e3c9b42c03f8ac0f75e07e2b46cb12244
DenysFi <figuradenis1@gmail.com>
2024-01-02 13:42:44 +0200

    FTX

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,25 @@
+const path = require('path');
+
 module.exports = {
-    // ...
-    module: {
-      rules: [
-        {
-          test: /\.scss$/,
-          use: [
-            {
-              loader: 'postcss-loader',
-              options: {
-                postcssOptions: {
-                  plugins: [
-                    require('autoprefixer')
-                  ]
-                }
-              }
+  // ... другие настройки ...
+
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          // ... другие лоадеры ...
+
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'FTX/img/footer/', // Измените эту строку
+              publicPath: (url) => path.join('FTX/img/footer/', url),
             },
-          ],
-        },
-      ],
-    },
-  };
+          },
+        ],
+      },
+    ],
+  },
+};
